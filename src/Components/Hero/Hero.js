74c7dc0a2aeb0d61c6c39ac41c669c7147f57930
ujin <rezashahtrader@gmail.com>
2023-05-20 17:21:01 +0330

    Hero

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <h4>در تخفیف بخرید </h4>
+            <h2>ارزانترین قیمت </h2>
+            <h1>با بهترین کیفیت</h1>
+            <p>با خرید از ما 70% پولتون رو پس انداز کنید </p>
+            <button>همین الان بخر</button>
+        </section>
+    )
+}

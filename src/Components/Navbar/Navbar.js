8d5f4c36956f1550af65a9017b5eada2497bbd92
ujin <rezashahtrader@gmail.com>
2023-05-20 17:04:18 +0330

    write navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,27 @@
 import React from "react";
+import "./Navbar.css";
+import logo from '../../img/logo.png';
 
 export default function Navbar() {
     return (
         <section className="Navbar">
-            <a href="#"><img src='./img/logo.png' className="logo"></img></a>
+            <a href="#">
+                <img src={logo} className="logo" alt="Logo" />
+            </a>
+            <div>
+                <ul className="navbar">
+                    <li><a className="active" href="farsi.html">خانه</a></li>
+                    <li><a href="#">فروشگاه</a></li>
+                    <li><a href="#">بلاگ</a></li>
+                    <li><a href="#">درباره ما</a></li>
+                    <li><a href="#">تماس با ما</a></li>
+                    <li><a href="#"><i class="fa-regular fa-bag-shopping"></i></a></li>
+                </ul>
+            </div>
         </section>
-    )
+    );
 }
+
 
 {/* <section id="header">
     <a href="#"><img src="img/logo.png" class="logo " alt=""></a>

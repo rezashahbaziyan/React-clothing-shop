d47d90571cdb9d752b7af57ac94673dd28f3112c
ujin <rezashahtrader@gmail.com>
2023-05-20 17:35:30 +0330

    Feature.js

---
+++ src/Components/Feature/Feature.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Feature() {
+    return (
+        <section id="feature" className="section-p1" dir="rtl">
+            <div className="fe-box">
+                <img src="../../img/features/f1.png" alt="" />
+                <h6>خرید آسان</h6>
+            </div>
+            <div className="fe-box">
+                <img src="../../img/features/f2.png" alt="" />
+                <h6>سفارش آنلاین</h6>
+            </div>
+            <div className="fe-box">
+                <img src="../../img/features/f3.png" alt="" />
+                <h6>خریدارزان</h6>
+            </div>
+            <div className="fe-box">
+                <img src="../../img/features/f4.png" alt="" />
+                <h6>مد روز </h6>
+            </div>
+            <div className="fe-box">
+                <img src="../../img/features/f5.png" alt="" />
+                <h6>همیشه بدرخشید</h6>
+            </div>
+            <div className="fe-box">
+                <img src="../../img/features/f6.png" alt="" />
+                <h6>پشتیبانی 24/7 </h6>
+            </div>
+        </section>
+    );
+}

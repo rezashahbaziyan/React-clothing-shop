47d2f83df6d800fafe8c9940e783a4ddcb6b2771
ujin <rezashahtrader@gmail.com>
2023-05-20 16:36:14 +0330

    add img

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <section className="Navbar">
+            <a href="#"><image></image></a>
+        </section>
+    )
+}
+
+{/* <section id="header">
+    <a href="#"><img src="img/logo.png" class="logo " alt=""></a>
+    <div>
+        <ul id="navbar">
+            <li><a class="active" href="farsi.html">خانه</a></li>
+            <li><a href="shopfarsi.html">فروشگاه</a></li>
+            <li><a href="blogfarsi.html">بلاگ</a></li>
+            <li><a href="aboutfarsi.html">درباره ما</a></li>
+            <li><a href="tamasbama.html">تماس با ما</a></li>
+            <li><a href="cart.html"><i class="fa-regular fa-bag-shopping"></i></a></li>
+        </ul>
+    </div>
+</section> */}

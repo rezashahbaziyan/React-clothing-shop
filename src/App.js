c00500517573e8564184eda3276bdd739bde1041
ujin <rezashahtrader@gmail.com>
2023-05-20 17:31:03 +0330

    rrtl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import "./style.css";
+import Hero from "./Components/Hero/Hero";
 
 export default function App() {
-    return <Navbar />
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
+    )
 }

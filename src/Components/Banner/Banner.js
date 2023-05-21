251051d76c646e94b1b25bd48cd3db9afec57b30
ujin <rezashahtrader@gmail.com>
2023-05-21 19:51:07 +0330

    banner

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+export default function Banner() {
+    return (
+        <section id="banner" class="section-m1">
+            <h4>ویژه !!</h4>
+            <h2>اجناسی که <span>30% off</span> - خوردند </h2>
+            <button class="normal">همین الان ببین</button>
+        </section>
+    )
+}

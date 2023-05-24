eedd2eeb9d42d4a7ccabe93de0fe0ed401e5a004
ujin <rezashahtrader@gmail.com>
2023-05-24 22:50:42 +0330

    x

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import logo from '../../img/logo.png';
+// import logo from '../../img/logo.png';
 
 export default function Navbar() {
     return (

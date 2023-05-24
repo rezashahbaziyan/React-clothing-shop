d9cc234f61f433f1d0f7e7ef31b061219e271a4b
ujin <rezashahtrader@gmail.com>
2023-05-24 07:53:41 +0330

    s

--- src/Components/avlabileProduct/AvalibelProduct.jsx
+++ src/Components/avlabileProduct/AvalibelProduct.jsx
@@ -6,7 +6,7 @@ import n4 from "../../img/products/n4.jpg"
 import n5 from "../../img/products/n5.jpg"
 import n6 from "../../img/products/n6.jpg"
 import n7 from "../../img/products/n7.jpg"
-// import n8 from "../../img/products/n8.jpg"
+import n8 from "../../img/products/n8.jpg"
 import Product from "../Product/Product";
 
 export default function AvalibelProduct(props) {
@@ -22,7 +22,7 @@ export default function AvalibelProduct(props) {
                 <Product brand="jordan" name="تیشرت مردانه تابستانی" price="99,000" imgUrl={n5} />
                 <Product brand="adidas" name="تیشرت مردانه تابستانی" price="15,000" imgUrl={n6} />
                 <Product brand="adidas" name="تیشرت مردانه تابستانی" price="155,000" imgUrl={n7} />
-                {/* <Product brand="adidas" name="تیشرت مردانه تابستانی" price="35,000" imgUrl={n8} /> */}
+                <Product brand="adidas" name="تیشرت مردانه تابستانی" price="35,000" imgUrl={n8} />
             </div>
         </section>
     )
